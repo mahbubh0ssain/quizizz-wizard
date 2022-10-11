26b4bcd234b615882af36bb31a46107d72379130
mahbubh0ssain <mahbubju48@gmail.com>
2022-10-11 22:48:52 +0600

    right wrong toast added

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Option.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Option = ({ option, rightFunc }) => {
+const Option = ({ option, rightWrongFunc, answer }) => {
+  const notify = () => toast(answer);
+
   return (
     <div>
       <div
-        onClick={() => rightFunc(option)}
+        onClick={() => {
+          notify();
+          rightWrongFunc(option);
+        }}
         className="opt border rounded-3 m-1 p-2"
       >
+        <ToastContainer />
+
         {option}
       </div>
     </div>
--- src/Components/Topic/Topic.js
+++ src/Components/Topic/Topic.js
@@ -1,6 +1,9 @@
+import "./Topic.css";
 import Option from "../Option/Option";
 import { EyeIcon } from "@heroicons/react/24/solid";
-import "./Topic.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
 
 const Topic = ({
   questions,
@@ -10,13 +13,15 @@ const Topic = ({
   setWrongCount,
 }) => {
   const { options, question, correctAnswer } = questions;
-
-  // const [right, setRight] = useState();
-  const rightFunc = (option) => {
+  const notify = () => toast(correctAnswer);
+  const [answer, setAnswer] = useState();
+  const rightWrongFunc = (option) => {
     if (option === correctAnswer) {
       setRightCount(rightCount + 1);
+      setAnswer("YAY! Your answer is right.");
     } else {
       setWrongCount(wrongCount + 1);
+      setAnswer("Oops! Your answer is wrong.");
     }
   };
   return (
@@ -25,12 +30,18 @@ const Topic = ({
         <div className="d-flex justify-content-between align-items-center bg-primary p-3">
           <h5 className="text-white">{question}</h5>
           <button className="btn">
-            <EyeIcon className="icon text-white"></EyeIcon>
+            <EyeIcon onClick={notify} className="icon text-white"></EyeIcon>
+            <ToastContainer />
           </button>
         </div>
         <div className="card-body text-primary row row-cols-lg-2 row-cols-md-1  m-0">
           {options.map((option, i) => (
-            <Option key={i} option={option} rightFunc={rightFunc}></Option>
+            <Option
+              key={i}
+              option={option}
+              answer={answer}
+              rightWrongFunc={rightWrongFunc}
+            ></Option>
           ))}
         </div>
       </div>
