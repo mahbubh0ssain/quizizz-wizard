134f108eb40064eb831272e053df37431f8ea533
mahbubh0ssain <mahbubju48@gmail.com>
2022-10-11 13:23:05 +0600

    toastify added

--- src/Components/Topic/Topic.js
+++ src/Components/Topic/Topic.js
@@ -1,13 +1,23 @@
 import Option from "../Option/Option";
-import { BeakerIcon } from "@heroicons/react/24/solid";
+import { EyeIcon } from "@heroicons/react/24/solid";
+import "./Topic.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Topic = ({ questions }) => {
   const { options, question, correctAnswer } = questions;
+  const notify = () => toast(correctAnswer);
+
   return (
     <div>
       <div className="card border-primary shadow mb-3">
-        <h5 className="card-header">
-          {question} <BeakerIcon className="h-6 w-6 text-blue-500" />
-        </h5>
+        <div className="d-flex justify-content-between align-items-center bg-primary p-3">
+          <h5>{question}</h5>
+          <button onClick={notify} className="btn">
+            <EyeIcon className="icon text-white"></EyeIcon>
+            <ToastContainer />
+          </button>
+        </div>
         <div className="card-body text-primary row row-cols-lg-2 row-cols-md-1  m-0">
           {options.map((option) => (
             <Option option={option}></Option>
